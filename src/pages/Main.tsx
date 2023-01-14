b88678f8b9653692887b79b04dd086bcaecd637b
Roman Bogoudinov <r.bogoudinov@mac.com>
2023-01-14 21:15:47 +0200

    cleanup

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,7 +1,7 @@
 
 import { Outlet } from "react-router";
 import Footer from "../components/footer/Footer";
-import Navbar from "../components/navbar/Navbar";
+import Navbar from "../components/navbar/navbar";
 import "../styles/css/styles.css";
 import Home from "./Home";
 
